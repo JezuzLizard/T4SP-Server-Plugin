069442dca96cc735eca5ebac1551b949d8ecc954
ineed bots <ineedbots@pbot.org>
2023-09-22 13:02:20 -0600

    cleanup

--- deps/premake/SQLiteCpp.lua
+++ deps/premake/SQLiteCpp.lua
@@ -1,34 +1,34 @@
 SQLiteCpp = {
-    source = path.join(dependencies.basePath, "SQLiteCpp"),
+	source = path.join(dependencies.basePath, "SQLiteCpp"),
 }
 
 function SQLiteCpp.import()
-    links { "SQLiteCpp" }
-    SQLiteCpp.includes()
+	links { "SQLiteCpp" }
+	SQLiteCpp.includes()
 end
 
 function SQLiteCpp.includes()
-    includedirs {
-        path.join(SQLiteCpp.source, "include"),
-        path.join(SQLiteCpp.source, "sqlite3")
-    }
+	includedirs {
+		path.join(SQLiteCpp.source, "include"),
+		path.join(SQLiteCpp.source, "sqlite3")
+	}
 end
 
 function SQLiteCpp.project()
-    project "SQLiteCpp"
-        language "C"
+	project "SQLiteCpp"
+		language "C"
 
-        SQLiteCpp.includes()
+		SQLiteCpp.includes()
 
-        files {
-            path.join(path.join(SQLiteCpp.source, "src"), "**.cpp"),
-            path.join(path.join(SQLiteCpp.source, "include"), "**.h"),
-            path.join(path.join(SQLiteCpp.source, "sqlite3"), "**.c"),
-            path.join(path.join(SQLiteCpp.source, "sqlite3"), "**.h")
-        }
+		files {
+			path.join(path.join(SQLiteCpp.source, "src"), "**.cpp"),
+			path.join(path.join(SQLiteCpp.source, "include"), "**.h"),
+			path.join(path.join(SQLiteCpp.source, "sqlite3"), "**.c"),
+			path.join(path.join(SQLiteCpp.source, "sqlite3"), "**.h")
+		}
 
-        warnings "Off"
-        kind "StaticLib"
+		warnings "Off"
+		kind "StaticLib"
 end
 
 table.insert(dependencies, SQLiteCpp)
